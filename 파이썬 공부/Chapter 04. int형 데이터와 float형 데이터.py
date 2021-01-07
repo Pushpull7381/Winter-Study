0827f07ec2c04e8cf8a7d8a3388c48a5d04ace4c
PUSH <pushpull7381@gmail.com>
2021-01-08 01:45:22 +0900

    code

---
+++ 파이썬 공부/Chapter 04. int형 데이터와 float형 데이터.py
@@ -0,0 +1,18 @@
+# num=1
+# num1=1.00000000000001
+# num2=1.1
+# print(num)
+# print(num1)
+# print(num2)
+# print(num1+num2) 
+
+# print(5/2)
+# print(5//2)
+# print(5%2)
+
+num1=1
+num2=1.8
+num1=float(num1)
+num2=int(num2)
+print(num1)
+print(num2)
