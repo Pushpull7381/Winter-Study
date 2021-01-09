974141f4cbaf635071391ca33b11d1444b95e4c7
PUSH <pushpull7381@gmail.com>
2021-01-09 20:33:35 +0900

    code

---
+++ 파이썬 공부/Chapter 07. True, False 그리고 if와 그 형제들.py
@@ -0,0 +1,39 @@
+# # # # result=2>1
+# # # # print(result)
+
+# # # # result=2<1
+# # # # print(result)
+
+# # # print(bool(2>1))
+# # # print(bool(1))
+# # # print(bool(0))
+
+# # A=2
+# # B=1
+# # if A>B:
+# #     print("A는 B보다 큽니다.")
+# # if A==B:
+# #     print("A와 B는 같습니다.")
+# # if A!=B:
+# #     print("A는 B와 같지 않습니다.")
+# # if A<B :
+# #     print("A는 B보다 작습니다.")
+
+# a='PushPull'
+# print(True and False)
+# print(True or False)
+# print(not False)
+# print('P' in a)
+# print('s' not in a)
+
+# st='PushPull'
+# print(st.isdigit())
+# print(st.isalpha())
+# print(st.startswith('P'))
+# print(st.endswith('P'))
+
+n=9
+if 8 < n < 11:
+    print("Correct!")
+else :
+    print("Wrong!")
