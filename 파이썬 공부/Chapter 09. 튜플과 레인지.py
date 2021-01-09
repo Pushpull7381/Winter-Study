cd6bb2aaf24d622ca77f938cd3f642ec52305d31
PUSH <pushpull7381@gmail.com>
2021-01-09 22:52:17 +0900

    Create Chapter 09. 튜플과 레인지.py

---
+++ 파이썬 공부/Chapter 09. 튜플과 레인지.py
@@ -0,0 +1,26 @@
+# # # # num=(1,2,3,4,5,6)
+# # # # num=num+(7,8,9,10)
+# # # # print(num)
+
+# # # # num=(1,2,3,4,5)
+# # # # num*=2
+# # # # print(num)
+
+# # # # num=(1,2,3,4,5)
+# # # # num=num[0:3]
+# # # # print(num)
+
+# # # li=[['push', 11], ['pull', 8], ['age', 22]]
+# # # li[2][1]=23
+# # # print(li)
+
+# # print(list((1,2,3)))
+# # print(tuple([4,5,6]))
+# # print(list('Push'))
+# # print(tuple('Pull'))
+
+# print(list(range(1,11,2)))
+# print(tuple(range(2,11,2)))
+
+print(list(range(10, 0, -2)))
+print(tuple(range(9, 0, -2)))
