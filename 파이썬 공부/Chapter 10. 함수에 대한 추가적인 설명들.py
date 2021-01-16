91add8670906b0d986eded87c608526ee16133f9
PUSH <pushpull7381@gmail.com>
2021-01-16 22:13:27 +0900

    Create Chapter 10. 함수에 대한 추가적인 설명들.py

---
+++ 파이썬 공부/Chapter 10. 함수에 대한 추가적인 설명들.py
@@ -0,0 +1,26 @@
+# # # def print_name(name):
+# # #     print('이름은:', name)
+# # # def main():
+# # #     print_name('PUSH')
+# # # main()
+
+# # def print_name_age(name, age):
+# #     print('이름은:', name)
+# #     print('나이는:', age)
+# # def main():
+# #     print_name_age('PUSH', 23)
+# # main()
+
+# def print_name_age(name='PUSH', age='23'):
+#     print('이름은:', name)
+#     print('나이는:', age)
+# def main():
+#     print_name_age(24)
+# main()
+
+def print_name_age(age, name='PUSH'):
+    print('이름은:', name)
+    print('나이는:', age)
+def main():
+    print_name_age(24)
+main()
