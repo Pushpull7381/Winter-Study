e3b3f3113999dc3cb866041256d90d5791c5eb2a
PUSH <pushpull7381@gmail.com>
2021-01-18 01:45:24 +0900

    .

---
+++ 파이썬 공부/Chapter 11. '모듈의 이해' 그리고 '수학 모듈' 이용하기.py
@@ -0,0 +1,54 @@
+# from circle import ar_circle
+# from circle import ci_circle
+
+# def main():
+#     r = float(input("반지름 입력: "))
+#     ar = ar_circle(r)
+#     print("넓이: ", ar) 
+#     ci = ci_circle(r)
+#     print("둘레: ", ci)
+
+# main()
+
+# from circle import ar_circle, ci_circle
+# def main():
+#     r = float(input("반지름 입력: "))
+#     ar = ar_circle(r)
+#     print("넓이: ", ar) 
+#     ci = ci_circle(r)
+#     print("둘레: ", ci)
+
+# main()
+
+# import circle as cc
+# def ar_circle(rad):
+#     print("넓이: ", rad * rad * 3.14)
+# def ci_circle(rad):
+#     print("둘레: ", rad * 2 * 3.14)
+
+# def main():
+#     r=float(input("반지름 입력: "))
+#     ar_circle(r)
+#     ci_circle(r)
+
+#     sum=cc.ar_circle(r)+cc.ci_circle(r)
+#     print("넓이와 둘레의 합: ", sum)
+
+# main()
+
+math.pi		# 파이
+math.e		# e
+math.sin(x)		# sin(x)
+math.cos(x)		# cos(x)
+math.tan(x)		# tan(x)
+math.asin(x)	# arsin(x)
+math.acos(x)	# arcos(x)
+math.atan(x)	# artan(x)
+math.log(x)		# ln(x)
+math.log10(x)	# log10(x)
+math.log2(x)	# log2(x)
+math.exp(x)		# e^x
+math.sqrt(x)	# √(x)
+math.fabs(x)	# ∣x∣
+math.degress(x)	# convert radians to degrees
+math.radians(x)	# convert degrees to radians
---
+++ 파이썬 공부/circle.py
@@ -0,0 +1,4 @@
+def ar_circle(rad):
+    return rad * rad * 3.14
+def ci_circle(rad):
+    return rad * 2 * 3.14
